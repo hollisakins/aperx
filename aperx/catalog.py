c544c616988a1684c4288ca063ee3b9cd8ec0ea4
Hollis Akins <hollis.akins@gmail.com>
2025-03-27 13:16:36 -0500

    ?

--- aperx/catalog.py
+++ aperx/catalog.py
@@ -1,15 +1,64 @@
 
+def gauss(x, A, mu, sigma):
+    return A * np.exp(-0.5*(x-mu)**2/sigma**2)
 
+from astropy.io import fits
+from scipy.optimize import curve_fit
+from astropy.stats import sigma_clipped_stats
+
+def _fit_pixel_distribution(data, sigma_upper=1.0, maxiters=3):
+    data = data.flatten()
+    data = data[np.isfinite(data)]
+
+    mean, median, std = sigma_clipped_stats(data, sigma=3.0, maxiters=maxiters)
+
+    bins = np.linspace(-7*std, 7*std, 100)
+    y, bins = np.histogram(data, bins=bins)
+    y = y/np.max(y)
+    p0 = [1, median, std]
+    bc = 0.5*(bins[1:]+bins[:-1])
+    popt, pcov = curve_fit(gauss, bc[bc<0], y[bc<0], p0=p0)
+
+    for i in range(maxiters):
+        p0 = popt
+        bins = np.linspace(-4*popt[2], 4*popt[2], 100)
+        y, bins = np.histogram(data, bins=bins)
+        y = y/np.max(y)
+        bc = 0.5*(bins[1:]+bins[:-1])
+        popt, pcov = curve_fit(gauss, bc[bc<sigma_upper*popt[2]], y[bc<sigma_upper*popt[2]], p0=p0)
+
+    mu = popt[1]
+    sigma = popt[2]
+    return mu, sigma
 
 class Catalog:
 
 
     def __init__(self, _):
+
+        # Validate things
+
+
+        # load science images and weight images
+        
+
         pass
 
+    def get_sci(self, band):
+        """
+        Load the science image for the given band using memory mapping.
 
+        Args:
+        - band (str): The band for which to load the science image.
 
-    
+        Returns:
+        - np.ndarray: The loaded science image.
+        """
+        file_path = self.science_images[band]
+        with fits.open(file_path, memmap=True) as hdul:
+            sci_image = hdul[0].data
+        return sci_image
+
     @property
     def all_images_exist(self):
         pass
@@ -25,3 +74,91 @@ class Catalog:
     @property
     def detection_image_exists(self):
         pass
+
+    def detect_sources(self):
+        pass
+
+    def get_auto_photometry(self):
+        pass
+
+
+
+    def _build_chisq_detection_image(
+        self, 
+        detection_bands: List[str], 
+        sigma_upper: float = 1.0,
+        maxiters: int = 3,
+        truncate: bool = True,
+    ):
+        """
+        Build a chi-squared detection image from a list of detection bands.
+
+        Args:
+        - detection_bands (List[str]): List of detection bands to use.
+        - sigma_upper (float): Sigma clipping threshold to esimate background rms.
+        - maxiters (int): Maximum number of iterations to use when fitting the pixel distribution.
+        - truncate (bool): Whether to truncate individual SNR maps at 0.
+        """
+
+        shapes = []
+        for band in detection_bands:
+            shapes.append(self.get_sci(band).shape)
+            # check that the detection bands are psf matched
+
+        if len(set(shapes)) > 1:
+            raise ValueError('All detection bands must have the same shape.')
+        shape = shapes[0]
+
+        snr_images = np.zeros((len(detection_bands), *shape))
+        chisq = np.zeros(shape)
+        for i, band in enumerate(detection_bands):
+            # make normalized science images by multiplying by square root of the weight images
+            normalized_image = self.get_sci(band) * np.sqrt(self.get_wht(band))
+            
+            # fit pixel distribution of normalized science images to a Gaussian to get the rms
+            _, rms = _fit_pixel_distribution(
+                normalized_image, 
+                sigma_upper = sigma_upper, 
+                maxiters = maxiters
+            )
+            
+            # make signal to noise images by dividing normalized science images by rms
+            snr = normalized_image / rms
+            if truncate:
+                snr[snr < 0] = 0
+            snr_images[i] = snr
+            
+        # make chi-sq images by summing in quadrature the signal to noise images
+        chisq = np.sum(np.power(snr_images, 2.))
+
+        return chisq
+
+    def _build_chimean_detection_image(
+        self, 
+        detection_bands, 
+        **kwargs
+    ):
+        """
+        Build a chi-mean detection image from a list of detection bands.
+
+        Args:
+        - detection_bands (List[str]): List of detection bands to use.
+        - **kwargs: Additional keyword arguments to pass to _build_chisq_detection_image.
+        """
+
+        # make chi-sq image
+        chisq = self._build_chisq_detection_image(detection_bands, **kwargs)
+
+        n = np.zeros(chisq.shape)
+        for band in detection_bands:
+            isvalid = np.isfinite(self.science_images[band]) & (self.weight_images[band] > 0)
+            n += isvalid
+
+        from scipy.special import gamma
+        mu = np.sqrt(2) * gamma((n+1)/2) / gamma(n/2)
+        
+        chi_mean = (np.sqrt(chisq) - mu) / np.sqrt(n - mu**2)
+
+        return chi_mean
+
+    
--- aperx/psf.py
+++ aperx/psf.py
@@ -1,95 +1,218 @@
 
+def _run_se(detec_image, weight_image, output_cat, vignet_size, sex_install=None):
+    randn = np.random.randint(0, 10000)
+    param_file = os.path.join(config.temp_path, f'psfex_cat_{randn}.param')
+    se_run_script = os.path.join(config.temp_path, f'se_run_script_{randn}.sh')
+
+    with open(param_file, 'w') as param:
+        param.write('NUMBER\n')
+        param.write('FLAGS\n')
+        param.write('X_IMAGE\n')
+        param.write('Y_IMAGE\n')
+        param.write('X2_IMAGE\n')
+        param.write('Y2_IMAGE\n')
+        param.write('XY_IMAGE\n')
+        param.write('ERRX2_IMAGE\n')
+        param.write('ERRY2_IMAGE\n')
+        param.write('ERRXY_IMAGE\n')
+        param.write('X_WORLD\n')
+        param.write('Y_WORLD\n')
+        param.write('X2_WORLD\n')
+        param.write('Y2_WORLD\n')
+        param.write('XY_WORLD\n')
+        param.write('ERRX2_WORLD\n')
+        param.write('ERRY2_WORLD\n')
+        param.write('ERRXY_WORLD\n')
+        param.write('ALPHA_J2000\n')
+        param.write('DELTA_J2000\n')
+        param.write('A_IMAGE\n')
+        param.write('B_IMAGE\n')
+        param.write('ERRA_IMAGE\n')
+        param.write('ERRB_IMAGE\n')
+        param.write('A_WORLD\n')
+        param.write('B_WORLD\n')
+        param.write('ERRA_WORLD\n')
+        param.write('ERRB_WORLD\n')
+        param.write('THETA_IMAGE\n')
+        param.write('ERRTHETA_IMAGE\n')
+        param.write('THETA_WORLD\n')
+        param.write('ERRTHETA_WORLD\n')
+        param.write('THETA_J2000\n')
+        param.write('ERRTHETA_J2000\n')
+        param.write('ELONGATION\n')
+        param.write('ELLIPTICITY\n')
+        param.write('FLUX_APER(1)\n')
+        param.write('FLUXERR_APER(1)\n')
+        param.write('MAG_APER(1)\n')
+        param.write('MAGERR_APER(1)\n')
+        param.write('FLUX_AUTO\n')
+        param.write('FLUXERR_AUTO\n')
+        param.write('MAG_AUTO\n')
+        param.write('MAGERR_AUTO\n')
+        param.write('FLUX_RADIUS\n')
+        param.write('KRON_RADIUS\n')
+        param.write('MU_MAX\n')
+        param.write('CLASS_STAR\n')
+        param.write('FWHM_WORLD\n')
+        param.write('SNR_WIN\n')
+        param.write(f'VIGNET({vignet_size},{vignet_size})\n')
+    
+    if sex_install is None:
+        sex_install = 'sex'
+    
+    with open(se_run_script, 'w') as script:
+        script.write('') #...
+        script.write(f'{sex_install} {detec_image},{detec_image}\ \n')
+        script.write(f'    -CATALOG_NAME {output_cat}\ \n')
+        script.write(f'    -CATALOG_TYPE FITS_LDAC\ \n')
+        script.write(f'    -PARAMETERS_NAME {param_file}\ \n')
+        script.write(f'    -DETECT_MINAREA 8\ \n')
+        script.write(f'    -DETECT_THRESH 3.0\ \n')
+        script.write(f'    -ANALYSIS_THRESH 2.5\ \n')
+        script.write(f'    -FILTER Y\ \n')
+        script.write(f'    -FILTER_NAME /home/hakins/COSMOS-Web/se++/conv/gauss_5.0_9x9.conv\ \n')
+        script.write(f'    -DEBLEND_NTHRESH 32\ \n')
+        script.write(f'    -DEBLEND_MINCONT 0.001\ \n')
+        script.write(f'    -CLEAN Y\ \n')
+        script.write(f'    -CLEAN_PARAM 1.0\ \n')
+        script.write(f'    -MASK_TYPE CORRECT\ \n')
+        script.write(f'    -WEIGHT_IMAGE {weight_image},{weight_image}\ \n')
+        script.write(f'    -WEIGHT_TYPE MAP_WEIGHT,MAP_WEIGHT\ \n')
+        script.write(f'    -RESCALE_WEIGHTS Y\ \n')
+        script.write(f'    -WEIGHT_GAIN N\ \n')
+        script.write(f'    -PHOT_APERTURES 150\ \n')
+        script.write(f'    -PHOT_AUTOPARAMS 2.5, 1.0\ \n')
+        script.write(f'    -PHOT_PETROPARAMS 2.0, 1.0\ \n')
+        script.write(f'    -PHOT_AUTOAPERS 0.1, 0.1\ \n')
+        script.write(f'    -PHOT_FLUXFRAC 0.5\ \n')
+        script.write(f'    -MAG_ZEROPOINT 28.9\ \n')
+        script.write(f'    -GAIN 1.0\ \n')
+        script.write(f'    -PIXEL_SCALE 0\ \n')
+        script.write(f'    -SEEING_FWHM 0.1\ \n')
+        scripts.write(f'   -BACK_TYPE AUTO\ \n')
+        scripts.write(f'   -BACK_SIZE 256\ \n')
+        scripts.write(f'   -BACK_FILTERSIZE 3\ \n')
+        scripts.write(f'   -BACKPHOTO_TYPE LOCAL\ \n')
+        scripts.write(f'   -BACKPHOTO_THICK 64\ \n')
+        scripts.write(f'   -BACK_FILTTHRESH 0.0\ \n')
+
+    subprocess.run(se_run_script)
+
+    os.remove(param_file)
+    os.remove(se_run_script)
+
+
+def _run_psfex(
+        input_catalog, 
+        output_filename, 
+        fwhm_min=1.5, 
+        fwhm_max=4.0,
+        max_ellip=0.1,
+        psf_size=301,
+        checkplot_fwhm=False,
+    ):
+    """
+    Run PSFEx on a catalog.
+    """
+    randn = np.random.randint(0, 10000)
+    psfex_run_script = os.path.join(config.temp_path, f'psfex_run_script_{randn}.sh')
+
+    with open(psfex_run_script_name, 'w') as script:
+        script.write(f'psfex {input_catalog}\ \n') # /softs/astromatic/psfex/3.22.1/bin/psfex $SEcat
+        script.write(f'      -c /home/hakins/PSFEx/default.psfex\ \n')
+        script.write(f'      -PSF_SIZE {psf_size},{psf_size}\ \n')
+        script.write(f'      -PSFVAR_DEGREES 0\ \n')
+        script.write(f'      -PSFVAR_NSNAP 1\ \n')
+        script.write(f'      -PSF_SAMPLING 1.0\ \n')
+        script.write(f'      -PSF_RECENTER Y\ \n')
+        script.write(f'      -BASIS_TYPE PIXEL_AUTO\ \n')
+        script.write(f'      -PSF_SUFFIX ".psf"\ \n')
+        script.write(f'      -PSF_ACCURACY 0.01\ \n')
+        script.write(f'      -SAMPLEVAR_TYPE NONE\ \n')
+        script.write(f'      -SAMPLE_VARIABILITY 0.3\ \n')
+        script.write(f'      -SAMPLE_AUTOSELECT YES\ \n')
+        script.write(f'      -SAMPLE_FWHMRANGE {fwhm_min},{fwhm_max}\ \n')
+        script.write(f'      -SAMPLE_MINSN {min_snr}\ \n')
+        script.write(f'      -SAMPLE_MAXELLIP {max_ellip}\ \n')
+        script.write(f'      -BASIS_NUMBER 20\ \n')
+        script.write(f'      -PHOTFLUX_KEY "FLUX_APER(1)"\ \n')
+        script.write(f'      -PHOTFLUXERR_KEY "FLUXERR_APER(1)"\ \n')
+        if checkplot_fwhm:
+            script.write(f'      -CHECKPLOT_TYPE SELECTION_FWHM\ \n')
+            script.write(f'      -CHECKPLOT_NAME {}\ \n')
+        script.write(f'      -OUTCAT_TYPE FITS_LDAC\ \n')
+        script.write(f'      -OUTCAT_NAME {output_filename}.cat\ \n')
+
+
+    subprocess.run(psfex_run_script)
+
+    os.remove(input_catalog)
+    os.remove(outcat)
+
+
+
 
 class PSF:
 
     @classmethod
-    def build(cls, image):
-        param_file = os.path.join(config.aperx_data_path, 'psfex_cat.param')
+    def load(cls, filepath):
+        psf = fits.open(filepath)[1].data['PSF_MASK'][0][0]
+        return cls(psf)
+
+    @classmethod
+    def build(cls, 
+        images: List[str], 
+        psf_size: int = 301,
+    ):
+        """
+        Build a PSF model from a list of images.
+        """
         
+        if not psf_size % 2:
+            raise ValueError('PSF size must be odd.')
+        
+        output_catalogs = []
+        for image in images:
+            if not os.path.exists(image):
+                raise FileNotFoundError(f'{image} does not exist.')
+            
+            if '_i2d.fits' in image:
+                raise ValueError('Cannot build PSFs from i2d files (atm).')
 
+            elif '_sci.fits' in image:
+                weight_image = image.replace('_sci.fits', '_wht.fits')
 
-        with open(se_run_script_name, 'w') as script:
-            script.write('') #...
-            script.write(f'sex {detec_image},{detec_image}\ \n')
-            script.write(f'    -c {config_file}\ \n')
-            script.write(f'    -CATALOG_NAME {output_cat}\ \n')
-            script.write(f'    -CATALOG_TYPE FITS_LDAC\ \n')
-            script.write(f'    -PARAMETERS_NAME {param_file}\ \n')
-            script.write(f'    -DETECT_MINAREA 8\ \n')
-            script.write(f'    -DETECT_THRESH 3.0\ \n')
-            script.write(f'    -ANALYSIS_THRESH 2.5\ \n')
-            script.write(f'    -FILTER Y\ \n')
-            script.write(f'    -FILTER_NAME /home/hakins/COSMOS-Web/se++/conv/gauss_5.0_9x9.conv\ \n')
-            script.write(f'    -DEBLEND_NTHRESH 32\ \n')
-            script.write(f'    -DEBLEND_MINCONT 0.001\ \n')
-            script.write(f'    -CLEAN Y\ \n')
-            script.write(f'    -CLEAN_PARAM 1.0\ \n')
-            script.write(f'    -MASK_TYPE CORRECT\ \n')
-            script.write(f'    -WEIGHT_IMAGE {weight_image},{weight_image}\ \n')
-            script.write(f'    -WEIGHT_TYPE MAP_WEIGHT,MAP_WEIGHT\ \n')
-            script.write(f'    -RESCALE_WEIGHTS Y\ \n')
-            script.write(f'    -WEIGHT_GAIN N\ \n')
-            script.write(f'    -PHOT_APERTURES 150\ \n')
-            script.write(f'    -PHOT_AUTOPARAMS 2.5, 1.0\ \n')
-            script.write(f'    -PHOT_PETROPARAMS 2.0, 1.0\ \n')
-            script.write(f'    -PHOT_AUTOAPERS 0.1, 0.1\ \n')
-            script.write(f'    -PHOT_FLUXFRAC 0.5\ \n')
-            script.write(f'    -MAG_ZEROPOINT 28.9\ \n')
-            script.write(f'    -GAIN 1.0\ \n')
-            script.write(f'    -PIXEL_SCALE 0\ \n')
-            script.write(f'    -SEEING_FWHM 0.1\ \n')
-            scripts.write(f'   -BACK_TYPE AUTO\ \n')
-            scripts.write(f'   -BACK_SIZE 256\ \n')
-            scripts.write(f'   -BACK_FILTERSIZE 3\ \n')
-            scripts.write(f'   -BACKPHOTO_TYPE LOCAL\ \n')
-            scripts.write(f'   -BACKPHOTO_THICK 64\ \n')
-            scripts.write(f'   -BACK_FILTTHRESH 0.0\ \n')
+            output_cat = image.replace('_sci.fits', '_psfex_secat.fits')
+            
+            # Run SExtractor on the image
+            _run_se(image, weight_image, output_cat, psf_size, sex_install=None):
 
-            # script.write(f'    -CHECKIMAGE_TYPE SEGMENTATION,APERTURES\ \n')
-            # script.write(f'    -CHECKIMAGE_NAME SEGMENTATION,APERTURES\ \n')
+            output_catalogs.append(output_cat)
 
-        subprocess.run(os.path.join(config.temp_path, se_run_script_name))
+        if len(output_catalogs) > 1:
+            # Merge catalogs, if more than one
+            # final_catalog = ...
+            # rename to remove tiling? 
+            pass
+        else:
+            final_catalog = output_catalogs[0]
 
+        # Run PSFEx on the final catalog
+        output_psf = final_catalog.replace('.fits', '_psf.fits')
 
-        fwhm_min=1.5
-        fwhm_max=4.0 #7.0 for LW
-        min_snr=8.0
-        psf_size = 301
+        _run_psfex(final_catalog, 
+            output_psf, 
+            fwhm_min=fwhm_min,
+            fwhm_max=fwhm_max,
+            max_ellip=max_ellip,
+            checkplot_fwhm=checkplot_fwhm,
+            psf_size=psf_size
+        )
 
-        with open(psfex_run_script_name, 'w') as script:
-            script.write(f'psfex {catalog}\ \n') # /softs/astromatic/psfex/3.22.1/bin/psfex $SEcat
-            script.write(f'      -c /home/hakins/PSFEx/default.psfex\ \n')
-            script.write(f'      -PSF_SIZE {psf_size},{psf_size}\ \n')
-            script.write(f'      -PSFVAR_DEGREES 0\ \n')
-            script.write(f'      -PSFVAR_NSNAP 1\ \n')
-            script.write(f'      -PSF_SAMPLING 1.0\ \n')
-            script.write(f'      -PSF_RECENTER Y\ \n')
-            script.write(f'      -BASIS_TYPE PIXEL_AUTO\ \n')
-            script.write(f'      -PSF_SUFFIX ".psf"\ \n')
-            script.write(f'      -PSF_ACCURACY 0.01\ \n')
-            script.write(f'      -SAMPLEVAR_TYPE NONE\ \n')
-            script.write(f'      -SAMPLE_VARIABILITY 0.3\ \n')
-            script.write(f'      -SAMPLE_AUTOSELECT YES\ \n')
-            script.write(f'      -SAMPLE_FWHMRANGE {fwhm_min},{fwhm_max}\ \n')
-            script.write(f'      -SAMPLE_MINSN {min_snr}\ \n')
-            script.write(f'      -SAMPLE_MAXELLIP 0.1\ \n')
-            script.write(f'      -BASIS_NUMBER 20\ \n')
-            script.write(f'      -PHOTFLUX_KEY "FLUX_APER(1)"\ \n')
-            script.write(f'      -PHOTFLUXERR_KEY "FLUXERR_APER(1)"\ \n')
-            # script.write(f'      -CHECKPLOT_TYPE SELECTION_FWHM\ \n')
-            # script.write(f'      -CHECKPLOT_NAME $dir_psfs'checkplot_fwhm'\ \n')
-            script.write(f'      -OUTCAT_TYPE FITS_LDAC\ \n')
-            script.write(f'      -OUTCAT_NAME {outcat}\ \n')
-
-
-        subprocess.run(os.path.join(config.temp_path, psfex_run_script_name))
-
-
-        psf = fits.open(psfpath)[1].data['PSF_MASK'][0][0]
+        psf = fits.open(output_psf)[1].data['PSF_MASK'][0][0]
         size = np.shape(psf)[0]
 
-        hdr = fits.open(imgpath)[0].header
-        wcs = WCS(hdr)
+        wcs = WCS(fits.getheader(images[0]))
 
         new_wcs = WCS(naxis=2)
         new_wcs.wcs.crpix = [size/2,size/2]
@@ -98,7 +221,10 @@ class PSF:
         new_wcs.wcs.crval = wcs.wcs.crval
         new_wcs.wcs.ctype = wcs.wcs.ctype
 
-        fits.writeto(psfpath.replace('.psf','.fits'), data=psf, header=new_wcs.to_header(), overwrite=True)
+        fits.writeto(output_psf.replace('.psf','.fits'), data=psf, header=new_wcs.to_header(), overwrite=True)
+
+        # Remove extraneous files
+        
 
 
         return cls(...)
